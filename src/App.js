81a1297a92634dbd0bb1c8d8657eab6f9bf63808
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-01-12 22:57:20 +0100

    First commit demo-app-routing

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center", padding: "20px" }}>Demo App Routing</h1>
+    </div>
+  );
+}
+
+export default App;
