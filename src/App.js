0ee4517b3da52eec16985860ec6826b5672541ce
Kamil <tomala.kamil@gmail.com>
2020-01-13 15:27:43 +0100

    Put components to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Users from "./components/Users/Users";
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ textAlign: "center", padding: "20px" }}>Demo App Routing</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
